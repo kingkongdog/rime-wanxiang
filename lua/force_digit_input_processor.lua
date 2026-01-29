b0caf4c5639218edd390c048c9e96ad5d7843b3b
ligang <kingkongdog9527@gmail.com>
2026-01-29 18:00:42 +0800

    feat: try

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -10,6 +10,10 @@ function M.func(key, env)
   local code = key.keycode
   local input_time_ms = rime_api.get_time_ms()
 
+  if key:release() then
+    return 2
+  end
+
   if code >= 48 and code <= 57 then
     if input_time_ms - env.last_input_time_ms > 70 then
       -- 这里不要用 key:repr()，否则可能推入 Shift+1
