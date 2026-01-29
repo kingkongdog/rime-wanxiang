fc0375919a85ea5557f85c8d6b9ca7f0f584d7c7
ligang <kingkongdog9527@gmail.com>
2026-01-29 12:31:52 +0800

    feat: try to fix digit input duplicate issue

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -1,13 +1,20 @@
 local M = {}
 
+function M.init(env)
+    env.last_input = ""
+end
+
 function M.func(key, env)
   local engine = env.engine
   local context = engine.context
   local code = key.keycode
+  local input = context.input or ""
 
-  if code >= 48 and code <= 57 then
+  if code >= 48 and code <= 57 and #env.last_input == #input then
     -- 这里不要用 key:repr()，否则可能推入 Shift+1
-    context:push_input(string.char(code))
+    local char = string.char(code)
+    context:push_input(char)
+    env.last_input = input .. char
     return 1
   end
 
