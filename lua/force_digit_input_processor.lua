cf61b9809f67f8250a6e658b8f9636eb94d76483
ligang <kingkongdog9527@gmail.com>
2026-01-29 16:45:35 +0800

    feat: complete logic

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -10,10 +10,12 @@ function M.func(key, env)
   local code = key.keycode
   local input_time_ms = rime_api.get_time_ms()
 
-  if code >= 48 and code <= 57 and input_time_ms - env.last_input_time_ms > 70 then
-    -- 这里不要用 key:repr()，否则可能推入 Shift+1
-    context:push_input(string.char(code))
-    env.last_input_time_ms = input_time_ms
+  if code >= 48 and code <= 57 then
+    if input_time_ms - env.last_input_time_ms > 70 then
+      -- 这里不要用 key:repr()，否则可能推入 Shift+1
+      context:push_input(string.char(code))
+      env.last_input_time_ms = input_time_ms
+    end
     return 1
   end
 
