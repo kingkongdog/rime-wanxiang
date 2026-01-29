36abd7fd80da23dbca507bd52b74018aec4a1a33
ligang <kingkongdog9527@gmail.com>
2026-01-29 15:24:35 +0800

    feat: try

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -10,14 +10,14 @@ function M.func(key, env)
   local code = key.keycode
   local input = context.input or ""
 
-  if not context:is_composing() then
-    local char = string.char(code)
-    context:push_input(char)
-    env.last_input = char
-    return 1
-  end
-
   if code >= 48 and code <= 57 then
+    if not context:is_composing() then
+      local char = string.char(code)
+      context:push_input(char)
+      env.last_input = char
+      return 1
+    end
+
     if #env.last_input == #input then
       -- 这里不要用 key:repr()，否则可能推入 Shift+1
       local char = string.char(code)
