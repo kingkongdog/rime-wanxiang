6008b6985b6531a226249e927c7368c84fe73d80
ligang <kingkongdog9527@gmail.com>
2026-01-30 23:49:56 +0800

    feat: fix number char issue

--- lua/force_digit_input_processor.lua
+++ lua/force_digit_input_processor.lua
@@ -4,6 +4,20 @@ function M.init(env)
     env.last_input_time_ms = 0
 end
 
+local function to_t9_number(char)
+  local t9_map = {
+    A = "2", B = "2", C = "2",
+    D = "3", E = "3", F = "3",
+    G = "4", H = "4", I = "4",
+    J = "5", K = "5", L = "5",
+    M = "6", N = "6", O = "6",
+    P = "7", Q = "7", R = "7", S = "7",
+    T = "8", U = "8", V = "8",
+    W = "9", X = "9", Y = "9", Z = "9",
+  }
+  return t9_map[char:upper()]
+end
+
 function M.func(key, env)
   if key:release() then
     return 1
@@ -12,17 +26,38 @@ function M.func(key, env)
   local engine = env.engine
   local context = engine.context
   local code = key.keycode
+  local char = string.char(code)
   local input_time_ms = rime_api.get_time_ms()
+  local delta = input_time_ms - env.last_input_time_ms
 
+  -- 解决在输入拼音过程中偶现数字直接上屏问题
+  -- 解决在输入拼音过程中偶现按一个键进入 input 两次的问题
   if code >= 48 and code <= 57 then
-    if input_time_ms - env.last_input_time_ms > 70 then
+    -- https://gemini.google.com/share/b0c0711a3ce4
+    if delta > 70 then
       -- 这里不要用 key:repr()，否则可能推入 Shift+1
-      context:push_input(string.char(code))
+      context:push_input(char)
       env.last_input_time_ms = input_time_ms
     else
       context:push_input("")
     end
+    return 1
+  end
 
+  -- 解决在快速输入拼音过程中，触发 swipe 事件，导致进入 input 的不是数字是字母的问题
+  -- 输入数字过程是比较快的，真正 swipe 的时候是比较慢的，以 300ms 为界。
+  if (code >= 97 and code <= 122) or (code >= 65 and code <= 90) then
+    if delta > 70 then
+      if delta < 300 then
+        context:push_input(to_t9_number(char))
+      else
+        -- 这里不要用 key:repr()，否则可能推入 Shift+1
+        context:push_input(char)
+      end
+      env.last_input_time_ms = input_time_ms
+    else
+      context:push_input("")
+    end
     return 1
   end
 
