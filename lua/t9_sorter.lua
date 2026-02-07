829e670b9015424ed968bc462de8dc042ffa68d6
ligang <kingkongdog9527@gmail.com>
2026-02-07 14:59:56 +0800

    feat: test

--- lua/t9_sorter.lua
+++ lua/t9_sorter.lua
@@ -1,9 +1,10 @@
 -- 功能：根据首个候选词的长度，动态调整后续单字的排序方式
 
 local function sort_filter(input)
-    for cand in input:iter() do
-        yield(utf8.len("李刚"))
-    end
+  local code = utf8.len("李刚")
+  for cand in input:iter() do
+    yield(Candidate("raw", 0, #code, code, ""))
+  end
 end
 
 return sort_filter
