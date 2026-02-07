6ae84038f9836ea9ce0075bf03d7aa666903c3e7
ligang <kingkongdog9527@gmail.com>
2026-02-07 15:12:16 +0800

    feat: test

--- lua/t9_sorter.lua
+++ lua/t9_sorter.lua
@@ -1,7 +1,8 @@
 -- 功能：根据首个候选词的长度，动态调整后续单字的排序方式
+local utf8 = require("utf8")
 
 local function sort_filter(input)
-  local code = utf8.len("李刚")
+  local code = _VERSION
   for cand in input:iter() do
     yield(Candidate("raw", 0, #code, code, ""))
   end
