e6aa63cf169fcec7a4bb6ebf1f8009c3f78dfc5f
ligang <kingkongdog9527@gmail.com>
2026-02-07 20:47:00 +0800

    feat: optimize

--- lua/t9_sorter.lua
+++ lua/t9_sorter.lua
@@ -84,7 +84,7 @@ local function t9_sorter(input)
     -- 按组平铺输出
     for _, group in ipairs(groups) do
         -- 分割线
-        local sep = "===================================="
+        local sep = "====================================================="
         yield(Candidate("raw", l[1]._start, l[1]._end, "--- " .. group[1].comment .. " ---", sep))
         for _, cand in ipairs(group) do
             yield(cand)
