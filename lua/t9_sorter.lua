bc1808d161a7a3344419b1e258b3bd7cc7be0d38
ligang <kingkongdog9527@gmail.com>
2026-02-09 03:23:41 +0800

    feat: restore the first algorithm

--- lua/t9_sorter.lua
+++ lua/t9_sorter.lua
@@ -2,37 +2,37 @@
 
 local sep = "====================================================="
 
--- local tone_map = {["ā"]="a",["á"]="a",["ǎ"]="a",["à"]="a",["ē"]="e",["é"]="e",["ě"]="e",["è"]="e",["ī"]="i",["í"]="i",["ǐ"]="i",["ì"]="i",["ō"]="o",["ó"]="o",["ǒ"]="o",["ò"]="o",["ū"]="u",["ú"]="u",["ǔ"]="u",["ù"]="u",["ü"]="v",["ǘ"]="v",["ǚ"]="v",["ǜ"]="v"}
--- local tone_level_map = {["ā"]=1,["á"]=2,["ǎ"]=3,["à"]=4,["ē"]=1,["é"]=2,["ě"]=3,["è"]=4,["ī"]=1,["í"]=2,["ǐ"]=3,["ì"]=4,["ō"]=1,["ó"]=2,["ǒ"]=3,["ò"]=4,["ū"]=1,["ú"]=2,["ǔ"]=3,["ù"]=4,["ü"]=1,["ǘ"]=2,["ǚ"]=3,["ǜ"]=4}
+local tone_map = {["ā"]="a",["á"]="a",["ǎ"]="a",["à"]="a",["ē"]="e",["é"]="e",["ě"]="e",["è"]="e",["ī"]="i",["í"]="i",["ǐ"]="i",["ì"]="i",["ō"]="o",["ó"]="o",["ǒ"]="o",["ò"]="o",["ū"]="u",["ú"]="u",["ǔ"]="u",["ù"]="u",["ü"]="v",["ǘ"]="v",["ǚ"]="v",["ǜ"]="v"}
+local tone_level_map = {["ā"]=1,["á"]=2,["ǎ"]=3,["à"]=4,["ē"]=1,["é"]=2,["ě"]=3,["è"]=4,["ī"]=1,["í"]=2,["ǐ"]=3,["ì"]=4,["ō"]=1,["ó"]=2,["ǒ"]=3,["ò"]=4,["ū"]=1,["ú"]=2,["ǔ"]=3,["ù"]=4,["ü"]=1,["ǘ"]=2,["ǚ"]=3,["ǜ"]=4}
 
-local char_order = {
-    ["a"] = 1, ["ā"] = 1.1, ["á"] = 1.2, ["ǎ"] = 1.3, ["à"] = 1.4,
-    ["b"] = 2,
-    ["c"] = 3,
-    ["d"] = 4,
-    ["e"] = 5, ["ē"] = 5.1, ["é"] = 5.2, ["ě"] = 5.3, ["è"] = 5.4,
-    ["f"] = 6,
-    ["g"] = 7,
-    ["h"] = 8,
-    ["i"] = 9, ["ī"] = 9.1, ["í"] = 9.2, ["ǐ"] = 9.3, ["ì"] = 9.4,
-    ["j"] = 10,
-    ["k"] = 11,
-    ["l"] = 12,
-    ["m"] = 13,
-    ["n"] = 14,
-    ["o"] = 15, ["ō"] = 15.1, ["ó"] = 15.2, ["ǒ"] = 15.3, ["ò"] = 15.4,
-    ["p"] = 16,
-    ["q"] = 17,
-    ["r"] = 18,
-    ["s"] = 19,
-    ["t"] = 20,
-    ["u"] = 21, ["ū"] = 21.1, ["ú"] = 21.2, ["ǔ"] = 21.3, ["ù"] = 21.4,
-    ["v"] = 22, ["ü"] = 22.05, ["ǖ"] = 22.1, ["ǘ"] = 22.2, ["ǚ"] = 22.3, ["ǜ"] = 22.4,
-    ["w"] = 23,
-    ["x"] = 24,
-    ["y"] = 25,
-    ["z"] = 26,
-}
+-- local char_order = {
+--     ["a"] = 1, ["ā"] = 1.1, ["á"] = 1.2, ["ǎ"] = 1.3, ["à"] = 1.4,
+--     ["b"] = 2,
+--     ["c"] = 3,
+--     ["d"] = 4,
+--     ["e"] = 5, ["ē"] = 5.1, ["é"] = 5.2, ["ě"] = 5.3, ["è"] = 5.4,
+--     ["f"] = 6,
+--     ["g"] = 7,
+--     ["h"] = 8,
+--     ["i"] = 9, ["ī"] = 9.1, ["í"] = 9.2, ["ǐ"] = 9.3, ["ì"] = 9.4,
+--     ["j"] = 10,
+--     ["k"] = 11,
+--     ["l"] = 12,
+--     ["m"] = 13,
+--     ["n"] = 14,
+--     ["o"] = 15, ["ō"] = 15.1, ["ó"] = 15.2, ["ǒ"] = 15.3, ["ò"] = 15.4,
+--     ["p"] = 16,
+--     ["q"] = 17,
+--     ["r"] = 18,
+--     ["s"] = 19,
+--     ["t"] = 20,
+--     ["u"] = 21, ["ū"] = 21.1, ["ú"] = 21.2, ["ǔ"] = 21.3, ["ù"] = 21.4,
+--     ["v"] = 22, ["ü"] = 22.05, ["ǖ"] = 22.1, ["ǘ"] = 22.2, ["ǚ"] = 22.3, ["ǜ"] = 22.4,
+--     ["w"] = 23,
+--     ["x"] = 24,
+--     ["y"] = 25,
+--     ["z"] = 26,
+-- }
 
 local function t9_sorter(input)
     local l = {}
@@ -102,24 +102,24 @@ local function t9_sorter(input)
         if group then
             table.insert(group.cands, cand)
         else
-            -- local clean_pinyin = group_pinyin:gsub("[%z\128-\255][\128-\191]*", tone_map)
-            -- local tone_level = tone_level_map[group_pinyin:match("[%z\128-\255][\128-\191]*")] or 0
-            -- local tone_pos = group_pinyin:find("[%z\128-\255][\128-\191]*") or 0
-            -- groupsMap[group_pinyin] = {pinyin_with_tone = group_pinyin, clean_pinyin = clean_pinyin, tone_level = tone_level, tone_pos = tone_pos, cands = { cand }}
+            local clean_pinyin = group_pinyin:gsub("[%z\128-\255][\128-\191]*", tone_map)
+            local tone_level = tone_level_map[group_pinyin:match("[%z\128-\255][\128-\191]*")] or 0
+            local tone_pos = group_pinyin:find("[%z\128-\255][\128-\191]*") or 0
+            groupsMap[group_pinyin] = {pinyin_with_tone = group_pinyin, clean_pinyin = clean_pinyin, tone_level = tone_level, tone_pos = tone_pos, cands = { cand }}
             -- 预处理：直接生成权重序列数字数组
-            local weight_seq = {}
-            for char in group_pinyin:gmatch(utf8.charpattern) do
-                local w = char_order[char]
-                if w then
-                    table.insert(weight_seq, w)
-                end
-            end
+            -- local weight_seq = {}
+            -- for char in group_pinyin:gmatch(utf8.charpattern) do
+            --     local w = char_order[char]
+            --     if w then
+            --         table.insert(weight_seq, w)
+            --     end
+            -- end
             
-            groupsMap[group_pinyin] = {
-                group_pinyin = group_pinyin,
-                weights = weight_seq, 
-                cands = { cand }
-            }
+            -- groupsMap[group_pinyin] = {
+            --     group_pinyin = group_pinyin,
+            --     weights = weight_seq, 
+            --     cands = { cand }
+            -- }
         end
     end
 
@@ -130,28 +130,28 @@ local function t9_sorter(input)
 
     -- 对组进行排序（只比较组里的第一个候选词）
     table.sort(groupsArr, function(a, b)
-        -- if a.clean_pinyin ~= b.clean_pinyin then
-        --     return a.clean_pinyin < b.clean_pinyin
-        -- end
-
-        -- if a.tone_pos ~= b.tone_pos then
-        --     return a.tone_pos > b.tone_pos
-        -- end
-
-        -- return a.tone_level < b.tone_level
-        local wa, wb = a.weights, b.weights
-        for i = 1, math.min(#wa, #wb) do
-            if wa[i] ~= wb[i] then
-                return wa[i] < wb[i]
-            end
+        if a.clean_pinyin ~= b.clean_pinyin then
+            return a.clean_pinyin < b.clean_pinyin
         end
-        return #wa < #wb
+
+        if a.tone_pos ~= b.tone_pos then
+            return a.tone_pos > b.tone_pos
+        end
+
+        return a.tone_level < b.tone_level
+        -- local wa, wb = a.weights, b.weights
+        -- for i = 1, math.min(#wa, #wb) do
+        --     if wa[i] ~= wb[i] then
+        --         return wa[i] < wb[i]
+        --     end
+        -- end
+        -- return #wa < #wb
     end)
 
     -- 按组平铺输出
     for _, group in ipairs(groupsArr) do
         -- 分割线
-        yield(Candidate("raw", l[1]._start, l[1]._end, "--- " .. group.group_pinyin .. " ---", sep))
+        yield(Candidate("raw", l[1]._start, l[1]._end, "--- " .. group.pinyin_with_tone .. " ---", sep))
         for _, cand in ipairs(group.cands) do
             yield(cand)
         end
