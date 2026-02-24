f93c3d8e06be423ceb1c83e8604a1f82d48409a6
ligang <kingkongdog9527@gmail.com>
2026-02-24 20:23:36 +0800

    Revert "feat: test"
    This reverts commit f8a4daef334832223498e70e5dd9de72108ca450.

--- lua/english_filter.lua
+++ lua/english_filter.lua
@@ -6,13 +6,12 @@ local function english_filter(input, env)
     for cand in input:iter() do
         -- 判断候选词是否来自你的英文翻译器 (wanxiang_english)
         -- 或者通过候选词内容是否包含英文字母来判断
-        yield(Candidate("raw", 0, #env.engine.context.input, cand.type, ""))
-        -- if (cand.type == "table" and #env.engine.context.input < min_length) then
-        --     -- 如果输入长度太短，则跳过该候选词（不显示）
-        --     goto continue
-        -- end
-        -- yield(cand)
-        -- ::continue::
+        if (cand.type == "table" and #env.engine.context.input < min_length) then
+            -- 如果输入长度太短，则跳过该候选词（不显示）
+            goto continue
+        end
+        yield(cand)
+        ::continue::
     end
 end
 
