09fd69156755306424417516c9a6974981956a41
ligang <kingkongdog9527@gmail.com>
2026-03-26 19:17:54 +0800

    feat: add lua/predict_patch_processor.lua

---
+++ lua/predict_patch_processor.lua
@@ -0,0 +1,24 @@
+-- 解决预测候选出现时，按空格会往 input 里 push 0 的问题
+
+local M = {}
+
+function M.func(key, env)
+    local engine = env.engine
+    local context = engine.context
+    local input = context.input
+    local keycode = key.keycode
+
+    if key:release() then
+        return 2
+    end
+
+    if context:is_composing() and string.find(input, "›") and keycode == 48 then
+        engine:commit_text(" ")
+        context:clear()
+        return 1
+    end
+
+    return 2
+end
+
+return M
