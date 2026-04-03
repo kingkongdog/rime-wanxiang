5f326cb70e508df04932c0fa2cbc4b2267b73858
ligang <kingkongdog9527@gmail.com>
2026-04-03 21:02:07 +0800

    feat: test

--- lua/t9_tone_processor.lua
+++ lua/t9_tone_processor.lua
@@ -8,7 +8,7 @@ local M = {}
 function M.init(env)
     env.select_notifier = env.engine.context.select_notifier:connect(function(ctx)
         if ctx.input:match("1[0-4]$") then
-            ctx.commit()
+            ctx:commit()
             ctx:clear()
         end
     end)
