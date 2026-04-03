537e5ec9218666cf2f7e0ee38b13b1838a8d9a0f
ligang <kingkongdog9527@gmail.com>
2026-04-03 20:22:57 +0800

    feat: test

--- lua/put_input_in_cand_list_when_empty_filter.lua
+++ lua/put_input_in_cand_list_when_empty_filter.lua
@@ -12,7 +12,7 @@ function M.func(input, env)
     -- 如果循环结束了 has_cand 仍为 false，说明原本没有候选词
     if not has_cand then
         -- context.input 包含了当前的编码
-        local code = env.engine.context:get_preedit()
+        local code = env.engine.context:get_preedit().text
         -- 构造一个简单的候选词：类型为 "raw"，起始位置 0，结束位置为输入长度
         yield(Candidate("raw", 0, #code, code, ""))
     end
