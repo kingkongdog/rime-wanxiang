3be7e8097f20e7449bc523d59d784ccfbe61c5e6
ligang <kingkongdog9527@gmail.com>
2026-04-04 01:51:24 +0800

    feat: 增加 input 末尾是 1 的功能

---
+++ lua/t9_input_length_filter.lua
@@ -0,0 +1,22 @@
+-- input 末尾是 1 时，要求候选词的拼音的长度与 input 的长度一致
+local function t9_input_length_filter(input, env)
+    local context = env.engine.context
+    local raw_input = context.input
+    
+    if raw_input:match("1$") and not raw_input:match("%d1$") then
+        for cand in input:iter() do
+            local pinyin = cand.comment or ""
+            
+            -- 计算 pinyin 长度不能用 #pinyin，否则带声调的元音的长度计算错误
+            if utf8.len(pinyin) == #raw_input - 1 then
+                yield(cand)
+            end
+        end
+    else
+        for cand in input:iter() do
+            yield(cand)
+        end
+    end
+end
+
+return t9_input_length_filter
--- lua/t9_tone_processor.lua
+++ lua/t9_tone_processor.lua
@@ -2,12 +2,13 @@
 -- input 末尾通过 11 12 13 14 10 表示声调
 -- t9_tone_filter 筛选候选词之后，select 候选词，候选词不会直接上屏，只会改变 preedit。比如选择了 "干"字，preedit 会变成 "干11"
 -- 这个 processor 注册 select_notifier，把选中的候选词直接上屏
+-- 增加 input 末尾是 1 功能
 
 local M = {}
 
 function M.init(env)
     env.select_notifier = env.engine.context.select_notifier:connect(function(ctx)
-        if ctx.input:match("1[0-4]$") then
+        if ctx.input:match("1[0-4]$") or (ctx.input:match("1$") and not ctx.input:match("%d1$")) then
             env.engine:commit_text(ctx:get_preedit().text:gsub("%d+‸$", ""))
             ctx:clear()
         end
