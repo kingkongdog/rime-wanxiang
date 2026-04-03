3b8ffd679d33206fc927dd1ed6c7f3f5a0e99692
ligang <kingkongdog9527@gmail.com>
2026-04-03 18:59:19 +0800

    feat: test

--- lua/t9_tone_filter.lua
+++ lua/t9_tone_filter.lua
@@ -15,7 +15,7 @@ local function t9_tone_filter(input, env)
     -- 1. 提取约束条件
     local target_tone = raw_input:match("1(%d)$")
 
-    yield(Candidate("raw", 0, #raw_input, target_tone, ""))
+    yield(Candidate("raw", 0, #raw_input, raw_input, ""))
 
     -- 2. 开始过滤候选词
     for cand in input:iter() do
