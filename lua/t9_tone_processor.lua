56fb0bfc18c7f5af9f71395cdfbf84697344d61b
ligang <kingkongdog9527@gmail.com>
2026-04-06 01:49:22 +0800

    fix: 逻辑错误

--- lua/t9_tone_processor.lua
+++ lua/t9_tone_processor.lua
@@ -15,16 +15,16 @@ function M.init(env)
         if not ctx.input:match("^%d+$") then
             return
         end
-
-        local preedit = ctx:get_preedit().text
-        if not preedit:match("%d") then
-            env.engine:commit_text(preedit:sub(1, -2))
-            ctx:clear()
-            return
-        end
-
+        
         if ctx.input:match("1[0-4]$") then
             ctx:pop_input(2)
+            
+            local preedit = ctx:get_preedit().text
+            if not preedit:match("%d") then
+                env.engine:commit_text(preedit:sub(1, -2))
+                ctx:clear()
+                return
+            end
         end
     end)
 end
