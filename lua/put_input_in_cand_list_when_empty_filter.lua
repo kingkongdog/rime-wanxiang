61a9f7c6b896dca1c3884f6aa6855b38a2f3b17d
ligang <kingkongdog9527@gmail.com>
2026-04-09 16:51:09 +0800

    feat: 输入数字通过 1 引导

--- lua/put_input_in_cand_list_when_empty_filter.lua
+++ lua/put_input_in_cand_list_when_empty_filter.lua
@@ -13,8 +13,17 @@ function M.func(input, env)
     if not has_cand then
         -- context.input 包含了当前的编码
         local code = env.engine.context.input
-        -- 构造一个简单的候选词：类型为 "raw"，起始位置 0，结束位置为输入长度
-        yield(Candidate("raw", 0, #code, code, ""))
+        -- 在 t9 界面下，想输入数字时，先输入1，比如想输入 234，按下 1234，preedit 会变成 1 > 234, 候选词变成 234
+        if code:match("^1") then
+            local text = code:sub(2)
+            local cand = Candidate("raw", 0, #text, text, "")
+            cand.preedit = "1 > " .. text
+            yield(cand)
+        else
+            -- 构造一个简单的候选词：类型为 "raw"，起始位置 0，结束位置为输入长度
+            yield(Candidate("raw", 0, #code, code, ""))
+        end
+
     end
 end
 
