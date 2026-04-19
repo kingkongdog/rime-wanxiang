6c1d240018d715e0140991b0a2c1cd6367d108ec
ligang <kingkongdog9527@gmail.com>
2026-04-20 02:56:18 +0800

    feat: 高频单字分割线改成 ===

--- lua/t9_sorter.lua
+++ lua/t9_sorter.lua
@@ -117,7 +117,8 @@ local function t9_sorter(input)
     end
 
     if first_len > 1 then
-        yield(Candidate("raw", l[1]._start, l[1]._end, "================ 高频单字 ================", ""))
+     -- yield(Candidate("raw", l[1]._start, l[1]._end, "================ 高频单字 ================", ""))
+        yield(Candidate("raw", l[1]._start, l[1]._end, "===", ""))
     end
 
     -- 高频单字去重
