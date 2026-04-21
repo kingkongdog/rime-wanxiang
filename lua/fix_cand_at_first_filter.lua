300038a7f66eaf8885205446d70fadfe9dcb8ff8
ligang <kingkongdog9527@gmail.com>
2026-04-21 19:39:36 +0800

    feat: 常用字固定在首位

---
+++ lua/fix_cand_at_first_filter.lua
@@ -0,0 +1,27 @@
+local map = {
+  l = "了",
+  d = "的",
+  b = "吧",
+  n = "呢",
+  m = "吗"
+}
+
+local function fix_cand_at_first_filter(input, env)
+  local rawInput = env.engine.context.input
+
+  local composition = env.engine.context.composition
+  if not composition:empty() then
+    local segment = composition:back()
+    local segment_input = rawInput:sub(segment._start + 1, segment._end)
+    local text = map[segment_input]
+    if text then
+      yield(Candidate("raw", segment._start, segment._end, text, ""))
+    end
+  end
+
+  for cand in input:iter() do
+    yield(cand)
+  end
+end
+
+return fix_cand_at_first_filter
