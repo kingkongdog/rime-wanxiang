1513b246c487a029ac3402b0b7db6c9220ec8453
ligang <kingkongdog9527@gmail.com>
2026-04-23 00:29:28 +0800

    feat: 单字母固定候选只在电脑上生效

--- lua/shortcuts_filter.lua
+++ lua/shortcuts_filter.lua
@@ -1,4 +1,6 @@
+local wanxiang = require("wanxiang/wanxiang")
 local privacy = require("./privacy")
+
 local jianma = {
   l = "了",
   d = "的",
@@ -25,14 +27,30 @@ local function deal_with_privacy(input, segment)
     end
 end
 
-local function shortcuts_filter(input, env)
+local M = {}
+
+function M.init(env)
+    env.is_t9 = false
+    if wanxiang.get_input_method_type then
+        local im_type = wanxiang.get_input_method_type(env)
+        if im_type == "t9" then
+            env.is_t9 = true
+        end
+    end
+end
+
+function M.func(input, env)
   local rawInput = env.engine.context.input
 
   local composition = env.engine.context.composition
   if not composition:empty() then
     local segment = composition:back()
     local segment_input = rawInput:sub(segment._start + 1, segment._end)
-    deal_with_jianma(segment_input, segment)
+
+    if not env.is_t9 then
+      deal_with_jianma(segment_input, segment)
+    end
+
     deal_with_privacy(rawInput, segment)
   end
 
@@ -41,4 +59,4 @@ local function shortcuts_filter(input, env)
   end
 end
 
-return shortcuts_filter
+return M
