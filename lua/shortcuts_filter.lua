7ddb005d7312ae562b730ffef8bed62c8c53a0af
ligang <kingkongdog9527@gmail.com>
2026-04-22 23:00:20 +0800

    feat: 加入 privacy 数据

--- lua/shortcuts_filter.lua
+++ lua/shortcuts_filter.lua
@@ -1,4 +1,5 @@
-local map = {
+local privacy = require("./privacy")
+local jianma = {
   l = "了",
   d = "的",
   b = "吧",
@@ -8,6 +9,22 @@ local map = {
   s = "是"
 }
 
+local function deal_with_jianma(input, segment)
+    local text = jianma[input]
+    if text then
+      yield(Candidate("raw", segment._start, segment._end, text, ""))
+    end
+end
+
+local function deal_with_privacy(input, segment)
+    local arr = privacy[input]
+    if arr then
+      for _, text in ipairs(arr) do
+        yield(Candidate("raw", segment._start, segment._end, text, ""))
+      end
+    end
+end
+
 local function shortcuts_filter(input, env)
   local rawInput = env.engine.context.input
 
@@ -15,10 +32,8 @@ local function shortcuts_filter(input, env)
   if not composition:empty() then
     local segment = composition:back()
     local segment_input = rawInput:sub(segment._start + 1, segment._end)
-    local text = map[segment_input]
-    if text then
-      yield(Candidate("raw", segment._start, segment._end, text, ""))
-    end
+    deal_with_jianma(segment_input, segment)
+    deal_with_privacy(segment_input, segment)
   end
 
   for cand in input:iter() do
