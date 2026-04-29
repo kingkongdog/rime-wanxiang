d2b6b21a02d17278e81475958b60bb5ce23a8b61
ligang <kingkongdog9527@gmail.com>
2026-04-29 20:09:08 +0800

    feat: 支持 emoji

--- lua/t9_tone_filter.lua
+++ lua/t9_tone_filter.lua
@@ -37,17 +37,23 @@ local function t9_tone_filter(input, env)
         return results
     end
 
-
-    local function yield_cand_by_tone_and_first_letter(cand, target_tone_array, target_first_letter_array)
-        local reserve = true
-
-        if not cand.comment or cand.comment == "" then
-            return
+    local comment_map = {}
+    local function check_cand_wanted(cand_comment, cand_pinyin_length, target_pinyin_length, target_tone_array, target_first_letter_array)
+        local is_wanted = comment_map[cand_comment]
+        if is_wanted then
+            return is_wanted
         end
 
-        local pinyin_array = split_by_space(cand.comment)
+        is_wanted = true
 
-        if reserve and target_tone_array then
+        if is_wanted and target_pinyin_length > 0 and cand_pinyin_length ~= target_pinyin_length then
+            comment_map[cand_comment] = false
+            return false
+        end
+
+        local pinyin_array = split_by_space(cand_comment)
+
+        if is_wanted and target_tone_array then
             for i, target_tone in ipairs(target_tone_array) do
                 if i > #pinyin_array then
                     break
@@ -55,18 +61,18 @@ local function t9_tone_filter(input, env)
 
                 local cand_vowel = pinyin_array[i]:match("[%z\128-\255][\128-\191]*")
                 if not cand_vowel then
-                    cand_vowel = cand.comment:match("([aoeiu])")    -- 注意 ü 在上面匹配到 -- 注意这个元音字母的排列顺序可是有讲究的。
+                    cand_vowel = cand_comment:match("([aoeiu])")    -- 注意 ü 在上面匹配到 -- 注意这个元音字母的排列顺序可是有讲究的。
                 end
                 local cand_tone = cand_vowel and vowel_tone_map[cand_vowel] or nil
                 -- 声调匹配校验
                 if target_tone ~= cand_tone then
-                    reserve = false
+                    is_wanted = false
                     break
                 end
             end
         end
 
-        if reserve and target_first_letter_array then
+        if is_wanted and target_first_letter_array then
             for i, target_first_letter in ipairs(target_first_letter_array) do
                 if i > #pinyin_array then
                     break
@@ -76,15 +82,14 @@ local function t9_tone_filter(input, env)
                 cand_first_letter = cand_first_letter and vowel_letter_map[cand_first_letter] or cand_first_letter or nil
                 -- 首字母匹配校验
                 if target_first_letter ~= cand_first_letter then
-                    reserve = false
+                    is_wanted = false
                     break
                 end
             end
         end
 
-        if reserve then
-            yield(cand)
-        end
+        comment_map[cand_comment] = is_wanted
+        return is_wanted
     end
 
     local function get_target_tone_array(input, prefix)
@@ -115,6 +120,19 @@ local function t9_tone_filter(input, env)
         return null
     end
 
+    local is_cand_wanted
+    local function yield_cand(cand, cand_pinyin_length, target_pinyin_length, target_tone_array, target_first_letter_array)
+        local comment = cand.comment
+        if comment == "" and is_cand_wanted then
+            yield(cand)
+        else if comment ~= "" then
+            is_cand_wanted = check_cand_wanted(comment, cand_pinyin_length, target_pinyin_length, target_tone_array, target_first_letter_array)
+            if is_cand_wanted then
+                yield(cand)
+            end
+        end
+    end
+
     -- 1. 提取约束条件
     -- 根据声调筛选
     local target_tone_prefix_0 = get_target_tone_array(raw_input, "0")
@@ -141,24 +159,26 @@ local function t9_tone_filter(input, env)
         local pure_input_length = #pure_input
         for cand in input:iter() do
             local pinyin = cand.comment:gsub("%s+", "")
+            -- 计算 pinyin 长度不能用 #pinyin，否则带声调的元音的长度计算错误
             local length = utf8.len(pinyin)
-            table.insert(cands, {cand, length})
+            table.insert(cands, { cand, length })
             if length > maxLength and length <= pure_input_length then  -- 有时候候选词长度会大于 input 长度，比如想输入囧， 按下 54664
                 maxLength = length
             end
         end
 
-        for i, cand in ipairs(cands) do
-            -- 计算 pinyin 长度不能用 #pinyin，否则带声调的元音的长度计算错误
-            if cand[2] == maxLength then
-                yield_cand_by_tone_and_first_letter(cand[1], target_tone, target_first_letter)
-            end
+        is_cand_wanted = false
+        for i, cand_obj in ipairs(cands) do
+            local cand = cand_obj[1]
+            local length = cand_obj[2]
+            yield_cand(cand, length, maxLength, target_tone, target_first_letter)
         end
     end
 
     if target_tone_prefix_1 or target_first_letter_prefix_1 then
+        is_cand_wanted = false
         for cand in input:iter() do
-            yield_cand_by_tone_and_first_letter(cand, target_tone, target_first_letter)
+            yield_cand(cand, nil, nil, target_tone, target_first_letter)
         end
     end
 end
