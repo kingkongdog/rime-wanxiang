43e0c979e1bd861e8fd9fa43c5c6dca89e996f32
ligang <kingkongdog9527@gmail.com>
2026-07-30 16:26:35 +0800

    feat: 9 键增加带圆圈的数字候选项

--- lua/prepend_digit_filter.lua
+++ lua/prepend_digit_filter.lua
@@ -1,21 +1,55 @@
 -- number_first.lua
 -- 检测输入是否是 1-9，并在第一位插入该数字
 
+local digit_map_1 = {
+    ["1"] = {"1", "①"},
+    ["2"] = {"2", "②"},
+    ["3"] = {"3", "③"},
+    ["4"] = {"4", "④"},
+    ["5"] = {"5", "⑤"},
+    ["6"] = {"6", "⑥"},
+    ["7"] = {"7", "⑦"},
+    ["8"] = {"8", "⑧"},
+    ["9"] = {"9", "⑨"},
+    ["0"] = {"0", "⓪"},
+}
+
+local digit_map_2 = {
+    ["1"] = {"1️⃣", "❶"},
+    ["2"] = {"2️⃣", "❷"},
+    ["3"] = {"3️⃣", "❸"},
+    ["4"] = {"4️⃣", "❹"},
+    ["5"] = {"5️⃣", "❺"},
+    ["6"] = {"6️⃣", "❻"},
+    ["7"] = {"7️⃣", "❼"},
+    ["8"] = {"8️⃣", "❽"},
+    ["9"] = {"9️⃣", "❾"},
+    ["0"] = {"0️⃣", "⓿"},
+}
+
+local function yield_digit_list(list)
+    for _, value in ipairs(list) do
+        local cand = Candidate("raw", 0, 1, value, "")
+        yield(cand)
+    end
+end
+
 local function filter(input, env)
     local context = env.engine.context
     local input_str = context.input
 
-    -- 使用正则判断是否为纯数字
-    if string.match(input_str, "^%d$") then
-        -- 构造一个候选词：类型为 "number"，内容为输入字符串，注释为 "数字"
-        local cand = Candidate("raw", 0, #input_str, input_str, "")
-        yield(cand)
-        
-        -- 之后正常输出其他的候选词
+    local is_length_one_digit = string.match(input_str, "^%d$")
+    if is_length_one_digit then
+        yield_digit_list(digit_map_1[input_str])
     end
-
+    
+    -- 之后正常输出其他的候选词
     for cand in input:iter() do
         yield(cand)
+    end
+
+    if is_length_one_digit then
+        yield_digit_list(digit_map_2[input_str])
     end
 end
 
